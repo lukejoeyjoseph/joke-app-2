6b304c6a398990fd357cb82aa5319a1b57ae1c56
Luke <lukejoey1976@gmail.com>
2021-11-30 23:52:33 +0800

    add index.js, add styles.css, add index.json, add index.html, connect to index.html

---
+++ index.js
@@ -0,0 +1,42 @@
+fetch("https://icanhazdadjoke.com/", {
+    headers: {Accept: "application/json"}
+})
+.then(res => res.json())
+.then(data => console.log(data))
+
+{
+    "attachments"; [
+      {
+        "fallback": "What kind of magic do cows believe in? MOODOO.",
+        "footer": "<https: icanhazdadjoke.com j HeaFdiyIJe|permalink> - <https: icanhazdadjoke.com|icanhazdadjoke.com>",
+        "text": "What kind of magic do cows believe in? MOODOO."
+      }
+    ],
+    "response_type"; "in_channel",
+    "username"; "icanhazdadjoke"
+  }
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-82372853-1', 'auto');
+    ga('send', 'pageview');
+
+    document.addEventListener('click', function (evt) {
+        if (evt.target.id === 'nav-toggle' || evt.target.parent.id === 'nav-toggle') {
+          document.getElementById('nav-menu').classList.toggle('is-active');
+        }
+      });
+
+      /* <![CDATA[ */
+        var google_conversion_id = 855658655;
+        var google_conversion_language = "en";
+        var google_conversion_format = "3";
+        var google_conversion_color = "ffffff";
+        var google_conversion_label = "gdOKCMjzmHAQn6GBmAM";
+        var google_remarketing_only = false;
+        /* ]]> */
+        
+        
